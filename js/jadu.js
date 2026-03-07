52a1f60abefecdb72ed5474f14fab7e1731935fc
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-03-07 20:02:14 +0600

    add issue details modal

--- js/jadu.js
+++ js/jadu.js
@@ -72,7 +72,7 @@ const displayAll = (issues) => {
     issues.forEach(elmnt => {
         const brdr = elmnt.status == "open" ? "border-green-500" : "border-purple-500";
         allSection.innerHTML += `
-        <div class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
+        <div onclick="my_modal_5.showModal(),mew(${elmnt.id})" class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
                 <div class="flex justify-between items-center">
                 ${elmnt.status == "open" ? `<img src="./assets/Open-Status.png">` : `<img src="./assets/Closed- Status .png">`}
 
@@ -119,7 +119,7 @@ const displayOpen = (issues) => {
         if(elmnt.status == "open"){
             kotore++;
         openSection.innerHTML += `
-        <div class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
+        <div onclick="my_modal_5.showModal(),mew(${elmnt.id})" class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
                 <div class="flex justify-between items-center">
                 ${elmnt.status == "open" ? `<img src="./assets/Open-Status.png">` : `<img src="./assets/Closed- Status .png">`}
 
@@ -170,7 +170,7 @@ const displayClose = (issues) => {
         if(elmnt.status == "closed"){
             kotore++;
         closeSection.innerHTML += `
-        <div class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
+        <div onclick="my_modal_5.showModal(),mew(${elmnt.id})" class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
                 <div class="flex justify-between items-center">
                 ${elmnt.status == "open" ? `<img src="./assets/Open-Status.png">` : `<img src="./assets/Closed- Status .png">`}
 
@@ -229,3 +229,59 @@ function toogle(id) {
     else
         document.getElementById('close-card-section').classList.remove('hidden');
 }
+
+
+function mew (url){
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${url}`)
+    .then(res => res.json())
+    .then(dtls => displayModal(dtls.data))
+}
+
+function displayModal (elmnt) {
+    console.log(elmnt);
+    const modal = document.getElementById('my_modal_5');
+    modal.innerHTML = "";
+    modal.innerHTML = `
+                <div class="modal-box">
+                    <h1 class="text-[#1F2937] font-bold text-2xl mb-1.5">${elmnt.title}</h1>
+                    <div class="flex justify-start items-center gap-1.5">
+                        <div class="text-[12px] font-medium text-white ${elmnt.status == 'open'? "bg-green-600": "bg-red-500"} py-1 px-2 rounded-2xl">${elmnt.status == 'open'? "Opened": "Closed"}</div>
+                        <p class="clr-drk text-[12px] font-medium"> • <span>${elmnt.status} by ${elmnt.author}</span> • <span>${elmnt.updatedAt}</span></p>
+                    </div>
+
+                     <div class="flex flex-wrap justify-start items-center gap-1 my-4">
+                        ${elmnt.labels.map(el =>
+                        `<div class="${el == "bug" ? "bug" :
+                            el == "help wanted" ? "help" :
+                                el == "enhancement" ? "enhance" :
+                                    el == "documentation" ? "dcmnt" :
+                                        "good"} py-1 px-2 rounded-xl text-[12px] font-medium flex justify-center border items-center gap-0.5">${el == "bug" ? `<img src="./assets/BugDroid.png">` :
+                                            el == "help wanted" ? `<img src="./assets/Lifebuoy.png">` :
+                                                el == "enhancement" ? `<img src="./assets/Sparkle.png">` : ""} ${el.toUpperCase()}</div>`).join("")}   
+                    </div>
+
+                    <p class="clr-drk">${elmnt.description}</p>
+                    <div class="flex justify-start gap-32 items-center my-7 bg-gray-50 p-3 rounded-lg">
+                        <div>
+                            <p class="clr-drk">Assignee:</p>
+                            <h4 class="font-semibold clr-blck">${elmnt.assignee}</h4>
+                        </div>
+                        <div class="">
+                            <p class="clr-drk">Priority:</p>
+                            <div class="${elmnt.priority == 'high'? "bg-red-400":
+                                elmnt.priority == 'medium'? "bg-amber-400": "bg-gray-400"
+                            } text-white py-1 px-4 rounded-2xl text-sm">
+                            ${elmnt.priority == 'high'? "HIGH":
+                                elmnt.priority == 'medium'? "MEDIUM": "LOW"
+                            }
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-action">
+                        <form method="dialog">
+                            <button class="btn btn-primary">Close</button>
+                        </form>
+                    </div>
+                </div>
+    `
+};
