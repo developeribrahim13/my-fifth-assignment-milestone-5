c53c15a6edf148cee97d6ea054b2687cb70e165c
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-03-07 16:52:17 +0600

    ft- add dynamic cards to open and close sections

--- js/jadu.js
+++ js/jadu.js
@@ -44,6 +44,25 @@ const pressAll = () => {
 pressAll();
 // all card er datake collect korlam
 
+// open card gulor jonno all data fetch korlam
+const pressOpen = () => {
+    toogle("open");
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then(res => res.json())
+        .then(issue => displayOpen(issue.data));
+}
+// open card gulor jonno all data fetch korlam
+
+// closed card gulor jonno all data fetch korlam
+const pressClose = () => {
+    toogle("close");
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then(res => res.json())
+        .then(issue => displayClose(issue.data));
+}
+// closed card gulor jonno all data fetch korlam
+
+
 
 // all card display korlam
 const displayAll = (issues) => {
@@ -90,6 +109,107 @@ const displayAll = (issues) => {
 }
 //  all card display korlam
 
+// open card guloke display korechi
+const displayOpen = (issues) => {
+     const openSection = document.getElementById('open-card-section');
+    openSection.innerHTML = "";
+    let kotore = 0;
+    issues.forEach(elmnt => {
+        const brdr = elmnt.status == "open" ? "border-green-500" : "border-purple-500";
+        if(elmnt.status == "open"){
+            kotore++;
+        openSection.innerHTML += `
+        <div class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
+                <div class="flex justify-between items-center">
+                ${elmnt.status == "open" ? `<img src="./assets/Open-Status.png">` : `<img src="./assets/Closed- Status .png">`}
+
+                ${elmnt.priority == "high" ? `<div class="text-red-500 bg-red-100 py-0.5 px-4 rounded-2xl text-sm font-medium">HIGH</div>
+                </div>`:
+                elmnt.priority == "medium" ? `<div class="text-amber-600 bg-amber-100 py-0.5 px-4 rounded-2xl text-sm font-medium">MEDIUM</div>
+                </div>`:
+                    `<div class="text-gray-500 bg-gray-100 py-0.5 px-4 rounded-2xl text-sm font-medium">LOW</div>
+                </div>` }
+
+                <h1 class="font-semibold clr-blck">${elmnt.title}</h1>
+
+                <p class="clr-drk text-[12px] font-normal">${elmnt.description}</p>
+
+                <div class="flex flex-wrap justify-start items-center gap-1">
+                ${elmnt.labels.map(el =>
+                        `<div class="${el == "bug" ? "bug" :
+                            el == "help wanted" ? "help" :
+                                el == "enhancement" ? "enhance" :
+                                    el == "documentation" ? "dcmnt" :
+                                        "good"} py-1 px-2 rounded-xl text-[12px] font-medium flex justify-center border items-center gap-0.5">${el == "bug" ? `<img src="./assets/BugDroid.png">` :
+                                            el == "help wanted" ? `<img src="./assets/Lifebuoy.png">` :
+                                                el == "enhancement" ? `<img src="./assets/Sparkle.png">` : ""} ${el.toUpperCase()}</div>`
+                    ).join("")}   
+                </div>
+
+                <hr class=" text-gray-200 my-3">
+
+                <p class="clr-drk text-[12px] ">#1 by <span>${elmnt.author}</span></p>
+                <p class="clr-drk text-[12px] ">${elmnt.createdAt}</p>
+            </div>
+        `    
+        }
+      document.getElementById('count').innerText = kotore;    
+    })
+}
+// open cardguloke display korechi
+
+
+
+// close card guloke display korechi
+const displayClose = (issues) => {
+     const closeSection = document.getElementById('close-card-section');
+    closeSection.innerHTML = "";
+    let kotore = 0;
+    issues.forEach(elmnt => {
+        const brdr = elmnt.status == "open" ? "border-green-500" : "border-purple-500";
+        if(elmnt.status == "closed"){
+            kotore++;
+        closeSection.innerHTML += `
+        <div class="bg-white p-3 border-t-3 ${brdr} rounded-xl space-y-1.5 shadow-sm shadow-gray-400 ">
+                <div class="flex justify-between items-center">
+                ${elmnt.status == "open" ? `<img src="./assets/Open-Status.png">` : `<img src="./assets/Closed- Status .png">`}
+
+                ${elmnt.priority == "high" ? `<div class="text-red-500 bg-red-100 py-0.5 px-4 rounded-2xl text-sm font-medium">HIGH</div>
+                </div>`:
+                elmnt.priority == "medium" ? `<div class="text-amber-600 bg-amber-100 py-0.5 px-4 rounded-2xl text-sm font-medium">MEDIUM</div>
+                </div>`:
+                    `<div class="text-gray-500 bg-gray-100 py-0.5 px-4 rounded-2xl text-sm font-medium">LOW</div>
+                </div>` }
+
+                <h1 class="font-semibold clr-blck">${elmnt.title}</h1>
+
+                <p class="clr-drk text-[12px] font-normal">${elmnt.description}</p>
+
+                <div class="flex flex-wrap justify-start items-center gap-1">
+                ${elmnt.labels.map(el =>
+                        `<div class="${el == "bug" ? "bug" :
+                            el == "help wanted" ? "help" :
+                                el == "enhancement" ? "enhance" :
+                                    el == "documentation" ? "dcmnt" :
+                                        "good"} py-1 px-2 rounded-xl text-[12px] font-medium flex justify-center border items-center gap-0.5">${el == "bug" ? `<img src="./assets/BugDroid.png">` :
+                                            el == "help wanted" ? `<img src="./assets/Lifebuoy.png">` :
+                                                el == "enhancement" ? `<img src="./assets/Sparkle.png">` : ""} ${el.toUpperCase()}</div>`
+                    ).join("")}   
+                </div>
+
+                <hr class=" text-gray-200 my-3">
+
+                <p class="clr-drk text-[12px] ">#1 by <span>${elmnt.author}</span></p>
+                <p class="clr-drk text-[12px] ">${elmnt.createdAt}</p>
+            </div>
+        `    
+        }
+      document.getElementById('count').innerText = kotore;    
+    })
+}
+// close cardguloke display korechi
+
+
 
 // button toogle korar jonno
 function toogle(id) {
@@ -97,4 +217,15 @@ function toogle(id) {
     document.getElementById('open').classList.remove('bg-blue-700', 'text-white');
     document.getElementById('close').classList.remove('bg-blue-700', 'text-white');
     document.getElementById(id).classList.add('bg-blue-700', 'text-white');
+    
+    
+    document.getElementById('all-card-section').classList.add('hidden');
+    document.getElementById('open-card-section').classList.add('hidden');
+    document.getElementById('close-card-section').classList.add('hidden');
+    if(id=='all')
+        document.getElementById('all-card-section').classList.remove('hidden');
+    else if(id=='open')
+        document.getElementById('open-card-section').classList.remove('hidden');
+    else
+        document.getElementById('close-card-section').classList.remove('hidden');
 }
